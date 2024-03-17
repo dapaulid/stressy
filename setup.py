6e112a1e75bc6442a7c67f8543a7d80dea067fcf
Daniel Pauli <dapaulid@gmail.com>
2024-03-17 22:38:22 +0100

    Remove setup.py
    It was not needed for editable installation with pip 24.0 and broke it for an earlier version.

--- setup.py
+++
@@ -1,6 +0,0 @@
-# for compatibility with legacy tools, actual config is in pyproject.toml
-# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-
-from setuptools import setup
-
-setup()
